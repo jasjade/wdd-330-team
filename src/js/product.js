bdb42fca41cb87de9191139558741884ef496d24
kaden530 <bro22001@byui.edu>
2023-01-18 13:15:29 -0700

    Team activity Week 3
    created product index, product details and moved the add to cart to the productDetails..mjs

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,9 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from '../json/ProductDetails.mjs';
+
 
 const dataSource = new ProductData('tents');
-
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
