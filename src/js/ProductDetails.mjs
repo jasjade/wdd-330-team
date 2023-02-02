766900e00e0ebf7cbc8d8fdf50627b846baf0ae7
Jeevee Greg Azores <60189319+gregazores@users.noreply.github.com>
2023-02-02 15:17:20 -0500

    Update ProductDetails.mjs

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage, returnCartItems, renderCartSuperscript } from '../js/utils.mjs';
+import { getLocalStorage, setLocalStorage, returnCartItems, renderCartSuperscript } from '../js/utils.mjs';
 
 //import { getLocalStorage, setLocalStorage } from '../js/utils.mjs';
 
