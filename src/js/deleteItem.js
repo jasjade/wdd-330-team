697626bce38bad36a9a850e9b140aa4a92965ab8
Natalia <nparedes517@yahoo.com>
2023-02-16 13:22:50 -0500

    fixed button listener  X in each item rendered

--- src/js/deleteItem.js
+++ src/js/deleteItem.js
@@ -1,27 +1,29 @@
-
-const remove = document.querySelector(".cart-remove__item");
-if (remove) {
-remove.addEventListener('click', deleteProduct);
+//store the all the X's existing in the cart 
+const remove = document.querySelectorAll(".cart-remove__item");
+//if there is an X present in the card, iterate as follows
+if (remove.length > 0) {
+    remove.forEach((button) => {
+    //add event listener to the item selected
+        button.addEventListener('click', (e) => {
+        //get the Id of the item selected from the DOM
+        const item = e.target.dataset.id;
+        //delete the item, pass it as argument to the deleteProduct function
+        deleteProduct(item);
+    });
+    })
 } else {
+    //if no X (length=0), display a message
     document.querySelector(".list-total").textContent = "Your cart is empty";
 }
 
- function deleteProduct() {
-    //get the parent element of the button that was clicked
-    //const productCard = event.target.closest(".cart-card");
-    //const productCard = document.querySelector(".cart-remove__item")
-    
-    //get the Id of the product
-    const itemIdRemoved = remove.dataset.id;
-    console.log(itemIdRemoved);
-    
+ function deleteProduct(itemIdRemoved) { 
     //remove the product from the localstorage
     let cart = JSON.parse(localStorage.getItem("so-cart"));
     cart = cart.filter((item) => item.Id !== itemIdRemoved);
     //set the localstorage
     localStorage.setItem("so-cart", JSON.stringify(cart));
 
-
+    //reload the page
     window.location.reload();
 }
 
